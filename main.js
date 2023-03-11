458a58b691aa200d951eb69145e837b5d98a7506
Z1mo0x <droweckiy@mail.ru>
2023-03-11 23:27:48 +0300

    counter for tek

---
+++ main.js
@@ -0,0 +1,33 @@
+const items = document.querySelectorAll('.item');
+let timer = new Date(2023, 6, 5, 8, 30, 0).getTime();
+
+
+function countDown() {
+    const now = new Date().getTime();
+    const distance = timer - now;
+    const hour = distance;
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    let days = Math.floor(distance / oneDay);
+    let hours = Math.floor((distance % oneDay) / oneHour);
+    let minutes = Math.floor((distance % oneHour) / oneMinute);
+    let seconds = Math.floor((distance % oneMinute) / 1000);
+
+    const values = [days, hours, minutes, seconds];
+
+
+    items.forEach((item, index) => {
+        item.textContent = values[index];
+        console.log(values[index]);
+    });
+
+}
+
+setInterval(countDown, 1000)
+
+
+
+// document.title = countdown;
